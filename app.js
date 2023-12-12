d3bb9626f9e0ff10e528c199d6bce5544eb79484
Carter Markham <cmarkham7272@gmail.com>
2023-12-12 12:43:08 -0600

    created start game and start timer functions. Start game button doesn't trigger any events or functions. Terminal says that the fontAwesome svg's are not recognizeable.

--- app.js
+++ app.js
@@ -1,16 +1,50 @@
+let pClock = document.getElementById('pClock')
+let startBtn = document.getElementById('startBtn')
+
+
+const startPieces = [
+  rook, knight, bishop, queen, king, bishop, knight, rook,
+  pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn, 
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn, 
+  rook, knight, bishop, king, queen, bishop, knight, rook
+]
+
 const startGame = () => {
-  console.log('start game');
+  let count = 6000
+  startBtn.addEventListener ('click', startTimer); 
+
+  const timer = setInterval(() => {
+    pClock.innerHTML = count;
+    count--;
+
+    console.log(count);
+
+    if (count === 0) {
+      clearInterval(timer);
+      endGame();
+    }
+  }, 1000);
+  
 }
 
 const endGame = () => {
   console.log('end game');
 }
 
+const startTimer = () => {
+  startGame()
+}
+
 const selectPiece = () => {
-  console.log('select piece');
+  
 }
 
 const movePiece = () => {
   console.log('move piece');
 }
+
 
