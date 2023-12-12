321318bf57090bbc27c5ee2f532588dbcfecd631
cmarkham7272@gmail.com <cmarkham7272@gmail.com>
2023-12-12 08:37:28 -0600

    added chess piece svg

---
+++ app.js
@@ -0,0 +1,16 @@
+const startGame = () => {
+  console.log('start game');
+}
+
+const endGame = () => {
+  console.log('end game');
+}
+
+const selectPiece = () => {
+  console.log('select piece');
+}
+
+const movePiece = () => {
+  console.log('move piece');
+}
+
